b8ddf2dae12de95989d6c6f7a0fd1a11a662ec04
Malina002 <malina.neag@student.upt.ro>
2023-03-30 18:44:25 +0300

    Modified menu for each command-line argument

--- Project.c
+++ Project.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 
 int checkArguments(int argc);
-void printMenu(char *options);
+void printFileTypeAndName(char *path);
+void menu_RegularFiles();
+void menu_Dir();
+void menu_SymLink();
+
 
 int checkArguments(int argc) {
     if(argc < 2) {
@@ -14,17 +21,61 @@ int checkArguments(int argc) {
     return 0;
 }
 
-void printMenu(char *options) {
-    printf("\n----  MENU ----\n");
-    printf("n: name\nm: last modification\na: access\n");
-    printf("Please, enter your options: ");
-    fgets(options, 20, stdin);
+// Function for printing the type, name and menu for the given arguments
+void printFileTypeAndName(char *path) {
+    
+    struct stat filestat;
+
+    printf("\n%s", path);
+    
+    if(lstat(path, &filestat) == -1) {
+        printf("Error: %s\n", strerror(errno));
+        return;
+    }
+
+    if(S_ISREG(filestat.st_mode)) {
+        printf("- REGULAR FILE\n");
+        menu_RegularFiles();
+    }
+
+    else if(S_ISDIR(filestat.st_mode)) {
+        printf(" - DIRECTORY\n");
+        menu_Dir();
+    }
+
+    else if(S_ISLNK(filestat.st_mode)) {
+        printf(" - SYMBOLIC LINK\n");
+        menu_SymLink();
+    }
+
+    else {
+        printf("File type not supported.\n");
+    }
+
 }
 
-int main(int argc, char *argv[]) {
-    
-    char options[20];
-    checkArguments(argc);
-    printMenu(options);
+void menu_RegularFiles(){
+    printf("\n----  MENU ----\n");
+    printf("-n: name\n-d: size\n-h: hard link count\n-m: time of last modification\n-a: access rights\n-l: create symbolic link\n");
+}
 
+void menu_SymLink(){
+    printf("\n----  MENU ----\n");
+    printf("-n: name\n-l: delete symbolic link\n-d: size of symbolic link\n-t: size of target file\n-a: access rights\n");
+}
+
+void menu_Dir(){
+    //not implemented yet
+}
+
+
+int main(int argc, char *argv[]) {
+
+    checkArguments(argc);
+
+    for(int i = 1; i < argc; i++) {
+        printFileTypeAndName(argv[i]);
+    }
+   
+    return 0;
 }
