5474b7c1d7e6e3288005c171da9e0e7e3e5d148d
Malina002 <malina.neag@student.upt.ro>
2023-05-10 22:59:40 +0300

    Made processes to run in parallel; Added pipes

--- Project.c
+++ Project.c
@@ -10,8 +10,16 @@
 #include <time.h>
 #include <dirent.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-int checkArguments(int argc) {
+/*
+TO BE FIXED until MONDAY:
+-the printing of the menu 
+-the printing to grades.txt, because the content is overwritten for each .C argument
+*/
+
+
+int checkArguments(int argc) {   
     if(argc < 2) {
         perror("Incorrect number of arguments! Usage: ./a.out <file/directory/link> ...\n");
         exit(EXIT_FAILURE);
@@ -37,6 +45,26 @@ void menu_SymbolicLink() {
     printf("\nPlease, enter your options: ");
 }
 
+void waitForChildren(){
+    
+    int status;
+    pid_t awaited_child;
+
+    awaited_child = wait(&status);
+
+    while(awaited_child > 0){
+        if(WIFEXITED(status)){
+            printf("\nChild process with PID %d has terminated normally with code %d.\n\n", awaited_child, WEXITSTATUS(status));
+        }
+        else{
+            printf("\nChild process with PID %d has terminated abnormally with code %d.\n\n", awaited_child, WEXITSTATUS(status));
+        }
+
+        awaited_child = wait(&status);
+    }
+
+}
+
 void printAccessRights(struct stat st) {
     
     printf("\nAccess rights:\n");
@@ -55,15 +83,175 @@ void printAccessRights(struct stat st) {
            (st.st_mode & S_IROTH) ? "yes" : "no",
            (st.st_mode & S_IWOTH) ? "yes" : "no",
            (st.st_mode & S_IXOTH) ? "yes" : "no");
+
 }
 
-void printRegularFileInfo(char *filepath) {
+void createNewTxtFile(char* dirpath, struct stat st) {
+    
+    pid_t pid;
+    
+    if((pid = fork()) < 0) {
+        perror("\nChild process creation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        char filename[1024];
+        snprintf(filename, sizeof(filename), "%s/%s_file.txt", dirpath, dirpath);
+        if (execvp("touch", (char *[]){ "touch", filename, NULL }) == -1) {
+            perror("\nError: execvp failed.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    else if(pid > 0) {
+        waitForChildren();
+    }
+
+}
+
+void setPermissions(char* linkpath, struct stat* st) {
+    
+    pid_t pid;
+
+    if((pid = fork()) < 0) {
+        perror("\nChild process creation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pid == 0) {
+        int check;
+        check = execlp("chmod", "chmod", "u+rwx,g+rw-x,o-rwx", linkpath, NULL);
+        if(check == -1){
+            perror(strerror(errno));
+            exit(errno);
+        }
+    }
+
+    else if(pid > 0) {
+        waitForChildren();
+    }
+}
+
+void compileCFile(char* filepath, struct stat st, int fd) {
+   
+    pid_t pid;
+    int pfd[2];
+
+    int hasCExtension = strstr(filepath, ".c") != NULL;
+
+    if(pipe(pfd) < 0) {
+        perror("\nPipe creation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if((pid = fork()) < 0) {
+        perror("\nChild process creation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    else if(pid == 0) {
+
+        close(pfd[0]);
+
+        if(hasCExtension) {
+            int check;
+            dup2(pfd[1], 1);
+            check = execlp("bash", "bash", "script.sh", filepath, NULL);
+            if(check == -1){
+                perror(strerror(errno));
+                exit(errno);
+            }   
+        }
+
+        if (!hasCExtension) {
+            printf("\n");
+            printf("(The argument is a regular file. In addition, the number of lines will be displayed: ");
+            
+            char command[100];
+            char* filename = strtok(filepath, "\n");
+            
+            snprintf(command, sizeof(command), "wc -l %s", filename);
+
+            FILE* wc_output = popen(command, "r");
+            
+            if (wc_output == NULL) {
+                perror(strerror(errno));
+                exit(errno);
+            }
+
+            int line_count = 0;
+
+            if (fscanf(wc_output, "%d", &line_count) != 1) {
+                perror(strerror(errno));
+                exit(errno);
+            }
+
+            if (pclose(wc_output) == -1) {
+                perror(strerror(errno));
+                exit(errno);
+            }
+
+            printf("%d)\n", line_count);
+        }
+        exit(EXIT_SUCCESS);
+    }
+
+    else if(pid > 0){
+
+        if(hasCExtension){
+                        
+            int errors = 0, warnings = 0, score=0;
+
+            close(pfd[1]);
+            
+            FILE* stream = fdopen(pfd[0],"r");;
+            
+            fscanf(stream, "%d %d", &errors, &warnings);
+            printf("Errors: %d\nWarnings: %d\n", errors, warnings);
+            
+            if(errors == 0 && warnings == 0){
+                score = 10;
+            }
+
+            if(errors >= 1){
+                score = 1;
+            }
+
+            if(errors == 0 && warnings > 10){
+                score = 2;
+            }
+
+            if(errors == 0 && warnings <= 10){
+                score = 2 + 8 * (10 - warnings) / 10;
+            }
+            
+            if (fd == -1) {
+                perror("Error opening file.\n");
+                exit(EXIT_FAILURE);
+            }
+              
+            char filename[1000];
+            snprintf(filename, sizeof(filename), "%s", filepath);
+            //dprintf(fd, "%s: %d\n", filename, score);
+            //close(fd);
+            //close(pfd[0]);
+            char string[1024];
+            sprintf(string, "%s : %d\n", filename, score);
+            write(fd, string, strlen(string));
+            close(fd);
+            close(pfd[0]);
+        }
+        waitForChildren();
+    }  
+}
+
+void printRegularFileInfo(char* filepath) {
     
     struct stat st;
 
     char options[20];
-    int numberOfOptions, hasCExtension, flag;
-    pid_t pid;
+    int numberOfOptions, flag;
     
     do {
         if(scanf("%20s", options) != 1) {
@@ -135,33 +323,9 @@ void printRegularFileInfo(char *filepath) {
                 break;
         }
     }
-    
-    hasCExtension = strstr(filepath, ".c") != NULL;
-
-    if (hasCExtension) {
-        
-        pid = fork();
-
-        if (pid < 0) {
-            perror("Fork failed.\n");
-            return;
-        } 
-        else if (pid == 0) {
-            execlp("./script.sh", "./script.sh", filepath, NULL);
-            exit(EXIT_SUCCESS);
-            return;
-        }
-        else {
-            int status, w;
-            w = wait(&status);
-           if(WIFEXITED(status)){
-               printf("(Child process with pid %d, exited with status %d)\n", w, WEXITSTATUS(status));
-           }
-        }
-    }
 }
 
-void printSymbolicLinkInfo(char *linkpath) {
+void printSymbolicLinkInfo(char* linkpath) {
     
     struct stat st;
     struct stat targetstat;
@@ -242,12 +406,11 @@ void printSymbolicLinkInfo(char *linkpath) {
     }
 }
 
-void printDirectoryInfo(char *dirpath) {
+void printDirectoryInfo(char* dirpath) {
 
     char options[20];
     int numberOfOptions, flag;
     struct stat st;
-    pid_t pid;
     
     do {
         if(scanf("%20s", options) != 1) {
@@ -321,49 +484,17 @@ void printDirectoryInfo(char *dirpath) {
                 break;
         }
     }
-
-    
-    pid = fork();
-
-    if (pid < 0) {
-        perror("Fork failed.\n");
-        return;
-    }
-    else if (pid == 0) {
-        char filename[1024];
-        snprintf(filename, sizeof(filename), "%s/%s_file.txt", dirpath, dirpath);
-
-        FILE *fp = fopen(filename, "w");
-        
-        if (fp == NULL) {
-            perror("Error creating file");
-            return;
-        }   
-        fclose(fp);
-        printf("\n------------------------------------\n");
-        printf("\nThe provided argument is a directory. In addition, a corresponding txt file will be created:");
-        printf("\nFile created: %s\n", filename);
-        //printf("\n------------------------------------\n");
-        exit(EXIT_SUCCESS);
-    }
-    else {
-            int status, w;
-            w = wait(&status);
-           if(WIFEXITED(status)){
-               printf("(Child process with pid %d, exited with status %d)\n", w, WEXITSTATUS(status));
-           }
-        }
 }
 
-void printArgumentsInfo(char *path) {
+void printArgumentsInfo(char* path) {
     
     struct stat st;
    
     printf("\n%s", path);
     
     if(lstat(path, &st) == -1) {
-        printf("Error: %s\n", strerror(errno));
-        return;
+        perror(strerror(errno));
+        exit(errno);
     }
 
     switch (st.st_mode & S_IFMT) {
@@ -389,42 +520,68 @@ void printArgumentsInfo(char *path) {
         default:
             printf("File type not supported.\n");
             break;
-
     }
+
 }
 
+void createProcesses(char* path) {
+    
+    struct stat st;
+   // int fd = open("grades.txt", O_RDWR | O_CREAT, S_IRWXU);
+     int fd = open("grades.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU );
 
-int main(int argc, char *argv[]) {
+    if(lstat(path, &st) == -1) {
+        perror(strerror(errno));
+        exit(errno);
+    }
+
+    switch (st.st_mode & S_IFMT) {
+     
+        case S_IFREG:
+            compileCFile(path, st, fd);
+            break;
+     
+        case S_IFDIR:
+            createNewTxtFile(path, st);
+            break;
+     
+        case S_IFLNK:
+            setPermissions(path, &st);
+            break;
+     
+        default:
+            printf("File type not supported.\n");
+            break;
+    
+    }
+            
+}
+
+int main(int argc, char* argv[]) {
+ 
+    pid_t pid;
 
     checkArguments(argc);
-    
-    int i, status;
 
-    for(i = 1; i < argc; i++) {
+    for(int i = 1; i < argc; i++) {
 
-        pid_t pid = fork();
-
-        if(pid < 0) {
-            perror("Fork failed.");
+        if((pid = fork()) < 0) {
+            perror("\nFork failed.\n");
             exit(EXIT_FAILURE);
         } 
         
         else if(pid == 0) {
-            // Child process
             printArgumentsInfo(argv[i]);
-            printf("\n");
-            printf("*******************************************");
             printf("\n");
             exit(EXIT_SUCCESS);
         }
         
-        else {
-            // Parent process
-            waitpid(pid, &status, 0);
+        else if(pid > 0) {          
+            createProcesses(argv[i]);
+            waitForChildren();            
         }
     }
-
-    return 0;
+    return 0;    
 }
 
 
